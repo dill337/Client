415d1e7d7a79aa2ea94575138875cf4d3012ee21
dill337 <64544519+dill337@users.noreply.github.com>
2020-10-27 15:49:05 -0500

    update favmovies

--- src/components/fav-movies/fav-movies.jsx
+++ src/components/fav-movies/fav-movies.jsx
@@ -1,2 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types
+import PropTypes from 'prop-types';
+
+import { MovieCard } from '../movie-card/movie-card';
+
+export function FavMovies(props) {
+
+  const { movies, title, }
+}
